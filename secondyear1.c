488583705cb868e19797077049e390929ed93508
harshitmis1508 <145057615+harshitmis1508@users.noreply.github.com>
2023-09-15 14:07:33 +0530

    Add files via upload

---
+++ secondyear1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a[10],n,i;
+    printf(" Enter the size of an array :");
+    scanf("%d",&n);
+    printf("Enter the array element");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    
+    }
+
+}
